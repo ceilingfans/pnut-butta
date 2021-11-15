1ba604e7dea8d43359c0bacf07958cda16e52321
awned <65061212+ceilingfans@users.noreply.github.com>
2021-11-15 22:32:48 +0800

    feat: function to check validity of options

--- include/GameOptions.hpp
+++ include/GameOptions.hpp
@@ -6,10 +6,15 @@
 
 struct GameOptions
 {
-    int playerCount;
-    int substringLength;
-    bool isAssist;
-    std::vector<std::string> options;
+    int playerCount = 0;
+    int substringLength = 0;
+    bool isAssist = false;
+    std::vector<std::string> options {};
 };
+
+bool isValidOption(const GameOptions &opt)
+{
+  return opt.playerCount != 0 || opt.substringLength != 0 || opt.options.empty();
+}
 
 #endif /* PNUTBUTTA_GAMEOPTIONS_H */
