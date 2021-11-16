d4e0600efab2923e9c6bb00e2067bec6e6a834d5
awned <65061212+ceilingfans@users.noreply.github.com>
2021-11-16 16:09:47 +0800

    fix: range check

--- src/GameOptions.cpp
+++ src/GameOptions.cpp
@@ -2,5 +2,11 @@
 
 bool isValidOption(const GameOptions& opt)
 {
-  return opt.playerCount != 0 && opt.substringLength != 0 && !opt.options.empty();
+  return opt.playerCount > 0 && opt.substringLength >= 2 && !opt.options.empty() && opt.options.size() >= 2;
 }
+
+/*
+ * playerCount > 0
+ * substringLength >= 2
+ * !options.empty()
+ */
