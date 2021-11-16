5251b9998e73cda84d628e1db97fb9b1f1fbf493
awned <65061212+ceilingfans@users.noreply.github.com>
2021-11-16 16:11:52 +0800

    chore: remove garbage

--- src/GameOptions.cpp
+++ src/GameOptions.cpp
@@ -4,9 +4,3 @@ bool isValidOption(const GameOptions& opt)
 {
   return opt.playerCount > 0 && opt.substringLength >= 2 && !opt.options.empty() && opt.options.size() >= 2;
 }
-
-/*
- * playerCount > 0
- * substringLength >= 2
- * !options.empty()
- */
