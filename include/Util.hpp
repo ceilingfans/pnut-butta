c948f02cc9cd8d8b3cef921d02f60fd6d6c98078
awned <65061212+ceilingfans@users.noreply.github.com>
2021-11-16 16:08:58 +0800

    feat: utility functions

---
+++ include/Util.hpp
@@ -0,0 +1,40 @@
+#ifndef PNUTBUTTA_UTIL_H
+#define PNUTBUTTA_UTIL_H
+
+#include <string>
+#include <vector>
+
+namespace Printer
+{
+    enum ErrorCodes
+    {
+      INTERNAL = 0,
+      ALREADY_ENTERED,
+      INSUFFICIENT_OPTIONS,
+      INSUFFICIENT_PLAYERS,
+      INVALID_OPTION,
+      NAN,
+      LENGTH_SHORT,
+    };
+
+    enum LossCodes
+    {
+      INVALID = 0,
+      OPTION,
+      CHOICE,
+    };
+
+    void think();
+    void error(ErrorCodes p_code);
+    void loss(const std::string &p_loser, LossCodes p_code);
+    void win(const std::string &p_winner);
+    void prompt(const std::string &p_prompt);
+    void options(const std::vector<std::string> &p_options);
+} /* Printer */
+
+void clearScreen();
+std::string joinString(const std::vector<std::string> &p_strings, const std::string& p_delimiter);
+bool contains(const std::string &p_string, const std::string &p_search);
+bool isNumber(const std::string &p_string);
+
+#endif /* PNUTBUTTA_UTIL_H */
